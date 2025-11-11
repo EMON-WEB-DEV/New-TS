a88935a7d5968dae25e3305e81e67640e0474b0d
EMON <jakariachowdhury2427@gmail.com>
2025-11-12 00:53:47 +0600

    first commit

---
+++ src/keyof.ts
@@ -0,0 +1,17 @@
+type Vehicle = {
+        name : string;
+        wheels : number;
+        isElectric : boolean;
+};
+
+
+type Vehicle2 =  Vehicle;
+
+
+const vehicaleType : Vehicle2 =
+{
+        name : 'Car',
+        wheels : 4,
+        isElectric : true,
+        //id:1234,
+}
